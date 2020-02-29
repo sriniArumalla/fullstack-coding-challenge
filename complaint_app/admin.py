a1d7aa93124f56a3d23c30ec49aa623fd5209e18
Srini Arumalla <srini.arumalla@gmail.com>
2020-02-29 13:31:54 -0800

    enabled django admin

--- complaint_app/admin.py
+++ complaint_app/admin.py
@@ -1,30 +1,6 @@
-from django.db import models
-from django.contrib.auth.models import User
-# Create your models here.
+from django.contrib import admin
+from complaint_app.models import UserProfile, Complaint
 
-# Create your models here.
-class UserProfile(models.Model):
-    """Database model for users in the system """
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    full_name = models.CharField(max_length=150, blank=True, default="")
-    district = models.CharField(max_length=5, blank=True, default="")
-    party = models.CharField(max_length=50, blank=True, default="", null=True)
-    borough = models.CharField(max_length=50, blank=True, default="")
-    def __str__(self):
-      return str(self.user)
-
-class Complaint(models.Model):
-    """Database model for Complaints """
-    unique_key = models.CharField(max_length=150, blank=True, default="")
-    account = models.CharField(max_length=10, blank=True, default="", null=True)
-    opendate = models.DateField(blank=True, null=True)
-    complaint_type = models.CharField(max_length=150, blank=True, default="", null=True)
-    descriptor = models.CharField(max_length=150, blank=True, default="", null=True)
-    zip = models.CharField(max_length=5, blank=True, default="", null=True)
-    borough = models.CharField(max_length=50, blank=True, default="", null=True)
-    city = models.CharField(max_length=50, blank=True, default="", null=True)
-    council_dist = models.CharField(max_length=10, blank=True, default="", null=True)
-    community_board = models.CharField(max_length=150, blank=True, default="", null=True)
-    closedate = models.DateField(blank=True, null=True)
-    def __str__(self):
-     return str(self.unique_key)
+# Register your models here.
+admin.site.register(Complaint)
+admin.site.register(UserProfile)
